0a3b5c3a9dd4f91f5326c5ee54be1ec4613a2b8f
Dan <lyash.dv@gmail.com>
2018-12-11 15:58:12 +0200

    Fixed sign-up-in

--- docs/index.php
+++ docs/index.php
@@ -2,63 +2,10 @@
 require "authorization/db.php";
 
 $data = $_POST;
-  if(isset($data['do_login']))
-  {
-    $user = R::findOne('users','login = ?', array($data['login']));
-    if($user)
-    {
-      //login exists
-      if(password_verify($data['password'], $user-> password)){
-        //vse horosho, loginim polzovatelya
-        $_SESSION['logged_user'] = $user;
-      }else
-      {
-        $errors[] = "Wrong password";
-      }
-    }else
-    {
-      $errors[] = "Couldn't find user with that login";
-    }
-
-    if(!empty($errors))
-    {
-      echo '<div style="color:red;">'.array_shift($errors).'</div><hr>';
-    }
-  }
-
-
-  if (isset($data['do_signup'])) {
-    //
-
-    $errors = array();
-    if (trim($data['login']) == '') {
-        $errors[] = 'Enter login!';
-    }
-
-    if ($data['password'] == '') {
-        $errors[] = 'Enter password!';
-    }
-
-    if ($data['password'] != $data['password_2']) {
-        $errors[] = 'Enter second password correctly!';
-    }
-
-    if (R::count('users','login = ?', array($data['login']))>0) {
-        $errors[] = 'User with that login already exist';
-    }
-
-    if (empty($errors)) {
-        $user = R::dispense('users');
-        $user->login = $data['login'];
-        $user->password = password_hash($data['password'], PASSWORD_DEFAULT);
-        R::store($user);
-
-    } else {
-        echo '<div style="color: red;">' . array_shift($errors) . '</div>';
-    }
-}
-
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html>
@@ -92,12 +39,42 @@ $data = $_POST;
           <div class="dialog-content">
 
             <div>
-                <h3 class="sign-up-in active" id="in">Sign in</h3>
+                <h3 class="sign-up-in" id="in">Sign in</h3>
                 <h3 class="sign-up-in" id="up">Sign up</h3>
             </div>
             <p class="description">Please write down your nickname and password</p>
             
             <div id="signin">
+              <?php
+
+
+              if(isset($data['do_login']))
+             {
+               $user = R::findOne('users','login = ?', array($data['login']));
+               if($user)
+               {
+                 //login exists
+                 if(password_verify($data['password'], $user-> password)){
+                   //vse horosho, loginim polzovatelya
+                   $_SESSION['logged_user'] = $user;
+                   echo "<script>window.location.href='index.php'</script>";
+                 }else
+                 {
+                   $errors[] = "Wrong password";
+                 }
+               }else
+               {
+                 $errors[] = "Couldn't find user with that login";
+               }
+            
+               if(!empty($errors))
+               {
+                echo '<div style="color:red;">'.array_shift($errors).'</div>';
+               }
+             }
+
+
+              ?>
             <form action = "index.php" method ="POST">
                 <p>Nickname</p>
                 <input type="text" name="login" value="<?php echo@$data['login']; ?>">
@@ -109,6 +86,44 @@ $data = $_POST;
           </div>
 
           <div class="dnone" id="signup">
+
+            <?php
+if (isset($data['do_signup'])) {
+    //
+
+    $errors = array();
+    if (trim($data['login']) == '') {
+        $errors[] = 'Enter login!';
+    }
+
+    if ($data['password'] == '') {
+        $errors[] = 'Enter password!';
+    }
+
+    if ($data['password'] != $data['password_2']) {
+        $errors[] = 'Enter second password correctly!';
+    }
+
+    if (R::count('users','login = ?', array($data['login']))>0) {
+        $errors[] = 'User with that login already exist';
+    }
+
+    if (empty($errors)) {
+        $user = R::dispense('users');
+        $user->login = $data['login'];
+        $user->password = password_hash($data['password'], PASSWORD_DEFAULT);
+        $user->moderator = false;
+        R::store($user);
+
+    } else {
+        echo '<div style="color: red;">' . array_shift($errors) . '</div>';
+    }
+}
+            ?>
+
+
+
+
             <form action = "index.php" method ="POST">
                 <p>Nickname</p>
                 <input type="text" name="login">
--- docs/js/index.js
+++ docs/js/index.js
@@ -1,18 +1,45 @@
 
+var signswitch = {};
+
 $( document ).ready(function() {
+
+
+    if(localStorage.getItem('signswitch')!=null){
+        signswitch=JSON.parse(localStorage.getItem('signswitch'));
+        console.log(signswitch);
+        signFunc(signswitch[0]);
+    }
+    else{
+        signFunc(true);
+    }
+
+
     console.log( "ready!" );
     $('#in').on("click", function() {
+        signFunc(true);
+        signswitch[0]=true;
+        localStorage.setItem('signswitch',JSON.stringify(signswitch));
+    });
+
+    $('#up').on("click", function() {
+        signFunc(false);
+        signswitch[0]=false;
+        localStorage.setItem('signswitch',JSON.stringify(signswitch));
+    });
+});
+
+function signFunc(b){
+    if(b){
         $('#in').addClass('active');
         $('#up').removeClass('active');
         $('#signup').addClass('dnone');
         $('#signin').removeClass('dnone');
-    });
-
-    $('#up').on("click", function() {
+    }else{
         $('#up').addClass('active');
         $('#in').removeClass('active');
         $('#signin').addClass('dnone');
         $('#signup').removeClass('dnone');
-    });
-});
+    }
+    
+}
 	
